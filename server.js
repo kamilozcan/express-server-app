eaab58da166ad3fbf4b845fe7e78c212b13cc0ff
Kamil Ozcan <kamilozcann@gmail.com>
2024-06-24 19:56:54 -0400

    Modified express tut

--- server.js
+++ server.js
@@ -1,15 +1,45 @@
 const express = require("express");
 const path = require("path");
+const { logger } = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler");
 
 const PORT = process.env.PORT || 3500;
+const cors = require("cors");
 const app = express();
 
-// Also you can install and import cors then you can use app.use - Has to be .js file
+//Custom middleware logger
+app.use(logger);
 
-// const cors = require("cors");
+//Cross Origin Resource Sharing
+//whitelist let the specific site access the backend
+const whitelist = [
+  "https://www.yousite.com",
+  "http://127.0.0.1:5500",
+  "http://localhost:3500/",
+];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
-// app.use(cors());
-// app.use(express.json());
+// tut07
+// built-in middleware to handle urlencoded data
+// in other words, form data
+// 'content-type: application/x-www-form-urlencoded'
+app.use(express.urlencoded({ extended: false }));
+
+// built-in middleware for json
+app.use(express.json());
+
+// serve static files
+app.use(express.static(path.join(__dirname, "/public")));
 
 // (.html)? means with or without .html
 app.get("^/$|/index(.html)?", (req, res) => {
@@ -26,16 +56,16 @@ app.get("/old-page(.html)?", (req, res) => {
 });
 
 //Route handlers
-// app.get(
-//   "/hello(.html)?",
-//   (req, res, next) => {
-//     console.log("attempted to load hello.html");
-//     next();
-//   },
-//   (req, res) => {
-//     res.send("Hello world!");
-//   }
-// );
+app.get(
+  "/hello(.html)?",
+  (req, res, next) => {
+    console.log("attempted to load hello.html");
+    next();
+  },
+  (req, res) => {
+    res.send("Hello world!");
+  }
+);
 
 //changing route handlers
 const one = (req, res, next) => {
@@ -53,8 +83,17 @@ const three = (req, res) => {
 
 app.get("/chain(.html)?", [one, two, three]);
 
-
-app.get("/*", (req, res) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ err: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
 });
+
+app.use(errorHandler);
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
